7a135422a35433f1259342ab7afc0532bf5a37ce
Flytreels <66539732+Flytreels@users.noreply.github.com>
2020-07-20 15:17:05 +0800

    Update deploy_linux.sh

--- app/src/main/assets/bin/include/core/deploy_linux.sh
+++ app/src/main/assets/bin/include/core/deploy_linux.sh
@@ -47,7 +47,7 @@ echo "- 正在执行附加操作"
 rm -rf $CONFIG_DIR/$confid/rootfs.conf
 echo "$rootfs2" >$CONFIG_DIR/$confid/rootfs.conf
 cp $TOOLKIT/cli.sh $rootfs2/
-cp $TOOLKIT/include $rootfs2/
+cp -R $TOOLKIT/include $rootfs2/
 rm -rf $CONFIG_DIR/$confid/cmd.conf
 echo "/bin/bash /cli.sh dropbear_start">$CONFIG_DIR/$confid/cmd.conf
 echo "- 正在解析系统信息"
