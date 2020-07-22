7d1d5744d922645442eb5a868321fd55f8b6c7c3
Flytreels <3021182972@qq.com>
2020-07-22 18:54:36 +0800

    💦

--- app/src/main/assets/exec_start.sh
+++ app/src/main/assets/exec_start.sh
@@ -2,15 +2,49 @@
 # DogeLand-AppShell-Init
 #
 script="$1"
+#
 # BasicEnv
+#
+export EXECUTOR_PATH=$({EXECUTOR_PATH})
+export TOOLKIT=$({TOOLKIT})
+export START_DIR=$({START_DIR})
 export TMPDIR=$START_DIR
-# AppFrameInit
-if [[ -f "$({TOOLKIT})/appframe_init.sh" ]]; then
-  source $({TOOLKIT})/appframe_init.sh
+export SDCARD_PATH=$({SDCARD_PATH})
+export PACKAGE_NAME=$({PACKAGE_NAME})
+#
+# DATA
+#
+export DATA2_DIR="$SDCARD_PATH/Android/data/$PACKAGE_NAME/"
+export CONFIG_DIR="$DATA2_DIR/config/"
+export platform=$(sh $TOOLKIT/cli.sh platform)
+#
+# RunEnv
+#
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$TOOLKIT/lib/$platform/"
+export PATH="$PATH:$TOOLKIT"
+export PREFIX=$START_DIR
+#
+# PRoot
+#
+export PROOT_TMP_DIR="$DATA2_DIR/cache/"
+export PROOT_LOADER="$TOOLKIT/lib/$platform/lib_loader.so"
+# x64 ?
+if [[ "$platform" != "x86_64" ]]
+then
+echo "">/dev/null
 else
-  echo "!无法初始化运行环境"
-  exit 255
+export PROOT_LOADER_32="$TOOLKIT/lib/$platform/lib_loader32.so"
 fi
+#
+# LoadConfig
+#
+export confid="$(cat $CONFIG_DIR/.id.conf)"
+export cmd=$(cat $CONFIG_DIR/$confid/cmd.conf)
+export rootfs=$(cat $CONFIG_DIR/$confid/rootfs.conf)
+export PATH2=$(cat $CONFIG_DIR/$confid/path.conf)
+#
+# Other or RunCmd
+#
 if [[ -f "$TOOLKIT/install_bin.sh" ]]; then
   sh $TOOLKIT/install_bin.sh
 fi
