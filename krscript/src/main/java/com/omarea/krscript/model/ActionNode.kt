4da6f26d48334354091bf2ffa9e38c3b44b2fcab
Your Name <you@example.com>
2020-07-27 19:12:31 +0800

    a

--- krscript/src/main/java/com/omarea/krscript/model/ActionNode.kt
+++
@@ -1,7 +0,0 @@
-package com.omarea.krscript.model
-
-import java.util.*
-
-class ActionNode : RunnableNode() {
-    var params: ArrayList<ActionParamInfo>? = null
-}
--- krscript/src/main/java/com/omarea/krscript/model/GroupNode.kt
+++
@@ -1,9 +0,0 @@
-package com.omarea.krscript.model
-
-class GroupNode : NodeInfoBase() {
-    // 分组标题
-    var separator: String = ""
-
-    var supported: Boolean = true
-    val children: ArrayList<NodeInfoBase> = ArrayList()
-}
