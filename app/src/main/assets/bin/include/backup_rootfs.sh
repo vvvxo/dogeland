f5ee5664f9ea355111b173c990b9085d4aeb96b3
Your Name <you@example.com>
2020-08-15 20:35:25 +0800

    fix

--- app/src/main/assets/bin/include/backup_rootfs.sh
+++ app/src/main/assets/bin/include/backup_rootfs.sh
@@ -9,12 +9,12 @@ stop_rootfs
 umount_part
 echo "- 正在导出系统"
 cd $rootfs/
-if [ ! -n "$dir" ]; then
-  tar czvf "$DATA2_DIR/backup.tgz" --exclude='./dev' --exclude='./sys' --exclude='./proc' --exclude='./mnt'  --exclude='./sdcard'  --exclude='./dogeland' -C "$rootfs/" . >/dev/null
-  echo "  已保存: $DATA2_DIR/backup.tgz"
+if [[ "$(pwd)" != "$rootfs" ]] && if [[ "$(pwd)" != "$rootfs/" ]]
+then
+echo "!无法切换到Rootfs目录,备份失败."
+exit 1
 else
-  tar czvf "$dir/backup.tgz" --exclude='./dev' --exclude='./sys' --exclude='./proc' --exclude='./mnt'  --exclude='./sdcard'  --exclude='./dogeland' -C "$rootfs/" . >/dev/null
-  echo "  已保存: $dir/backup.tgz"
+tar czvf "$DATA2_DIR/backup.tgz" --exclude='./dev' --exclude='./sys' --exclude='./proc' --exclude='./mnt'  --exclude='./sdcard'  --exclude='./dogeland' -C "$rootfs/" . >/dev/null
 fi
 echo "- 完成"
 
