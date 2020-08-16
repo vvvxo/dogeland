d4691c4844569f36dfdbf4074ee7d47fc7e9f2cf
Your Name <you@example.com>
2020-08-16 13:17:38 +0800

    FixAptEnv

--- app/src/main/assets/bin/include/set_env.sh
+++ app/src/main/assets/bin/include/set_env.sh
@@ -7,6 +7,6 @@ set_env(){
 unset TMP TEMP TMPDIR LD_PRELOAD LD_DEBUG
 HOME=/root
 LANG=C.UTF-8
-PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games
+PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games:/usr/local/sbin:/sbin
 PATH=$PATH:$PATH2
 }
