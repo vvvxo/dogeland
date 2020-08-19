1a5cbae370a0ff37a4c1d0108087e9d93fb7ffd6
Flytreels <3021182972@qq.com>
2020-08-19 20:24:32 +0800

    fixStopRootfs

--- app/src/main/assets/bin/include/stop_rootfs.sh
+++ app/src/main/assets/bin/include/stop_rootfs.sh
@@ -4,11 +4,18 @@
 # license: GPL-v2.0
 #
 stop_rootfs(){
+if [[ "$(cat $rootfs/dogeland/status)" != "Run" ]]
+then
+echo "">/dev/null
+else
+
+# Change Status
+rm -rf $rootfs/dogeland/status
+echo "Stop">$rootfs/dogeland/status
 pkill sshd && pkill dropbear
 pkill vncserver
 pkill ash && pkill zsh && pkill bash
 pkill sh && pkill chroot && pkill proot
-# Change Status
-rm -rf $rootfs/dogeland/status
-echo "Stop">$rootfs/dogeland/status
+
+fi
 }
